799eaef70bdc96a0337dd67cef830026f7e1d0e5
Aditya301-raj <rajadit016@gamil.com>
2022-08-03 23:32:06 +0530

    Star Pattran program

---
+++ Chapter_Pattern/StarPattern2.java
@@ -0,0 +1,35 @@
+package Chapter_Pattern;
+/* print Star pattern of given below by using for loop:
+  ******
+  *****
+  ****
+  **
+  *
+ */
+import java.util.Scanner;
+
+
+ public class StarPattern2 {
+    public static void main(String[] args) {
+        for(int i=5; i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.printf("*");
+            }
+            System.out.printf("\n");
+        }
+  
+
+
+     /* We can also take input from users by using Scanner function*/
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter the size of rows");
+      int N= sc.nextInt();
+      for(int i=N; i>=1;i--){
+        for(int j=1;j<=i;j++){
+            System.out.printf("*");
+        }
+        System.out.printf("\n");
+    }
+
+    }   
+ }
